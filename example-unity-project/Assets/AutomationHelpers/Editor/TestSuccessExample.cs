6b38498ff390cc2e9151e7b4557561a84ae06c11
david.noorgames <david.noorgames@gmail.com>
2017-05-12 17:05:21 +0400

    experimenting with test examples

---
+++ example-unity-project/Assets/AutomationHelpers/Editor/TestSuccessExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestSuccessExample {
+
+	[Test]
+	public void TestSuccessExampleSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator TestSuccessExampleWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        Assert.IsTrue(3 == 3);
+        yield return null;
+	}
+}
